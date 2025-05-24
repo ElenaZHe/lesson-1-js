7e3361449914a0bcb9b72d5eaff1931b845ef005
ElenaZHe <elena10020223@gmail.com>
2025-05-24 16:05:35 +0300

    fix-7-lesson

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -11,7 +11,7 @@
 
 function truncate(str, maxLength) {
   // your code
-  if(str>maxLength){ // проверка на длинну символов
+  if(str.length>maxLength){ // проверка на длинну символов
     return str.slice(0, maxLength) + "..."; // если у нас строка больше длины, то обрезаем ее
   }else{
     return str;  // если меньше возвращаем полную строку
